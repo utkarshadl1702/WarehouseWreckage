0ab51d01fd21bb13956f8d42b5770de131c3fa23
utkarshadl1702 <81684080+utkarshadl1702@users.noreply.github.com>
2022-07-22 23:27:09 +0530

    Ware House Wreckage
    An Unreal Engine Project made with blueprints

---
+++ Source/WarehouseWreckage/WarehouseWreckageGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarehouseWreckageGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAREHOUSEWRECKAGE_API AWarehouseWreckageGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
